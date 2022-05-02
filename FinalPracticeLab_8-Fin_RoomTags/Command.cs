193e5cb06ad1af0ec70f62501010a5ffa85239b8
HinaChleck <HinaChleck@gmail.com>
2022-05-02 21:17:28 +0500

    Добавьте файлы проекта.

---
+++ FinalPracticeLab_8-Fin_RoomTags/Command.cs
@@ -0,0 +1,72 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalPracticeLab_8_Fin_RoomTags
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            FamilySymbol familySymbol = new FilteredElementCollector(doc)//ищем семейство нужных марок в проекте
+                    .OfClass(typeof(FamilySymbol))
+                    .OfCategory(BuiltInCategory.OST_RoomTags)//Категоря марок верная, т.к. копировала стандартное семейство
+                    .OfType<FamilySymbol>()
+                    .Where(x => x.Name.Equals("Марка помещения Этаж - помещение"))
+                    .Where(x => x.FamilyName.Equals("M_Марка помещения_Этаж_Номер_Учеба"))
+                    .FirstOrDefault();
+
+            if (familySymbol == null)//проверка, заггружено ли семейство отверстий в проект
+            {
+                TaskDialog.Show("Ошибка", "Не найдено семейство марок. Загрузите семейство в проект");
+                return Result.Cancelled;
+            }
+
+            List<Level> listLevel = GetAssociatedLevels.ToViewPlans(doc);//отбор только тех уровней, у которых есть ассоциированнные виды. Без этого выдает ошибку, если не все уровни в проекте ассоциированы
+
+            List<List<ElementId>> allRooms = new List<List<ElementId>>();
+
+            Transaction transaction = new Transaction(doc);
+            transaction.Start("Построение помещений");
+
+            foreach (Level level in listLevel)
+            {
+                ICollection<ElementId> roomsIdsOnLevel = doc.Create.NewRooms2(level);
+
+                allRooms.Add(roomsIdsOnLevel as List<ElementId>);
+
+                foreach (ElementId roomId in roomsIdsOnLevel)
+                {
+                    Room room = doc.GetElement(roomId) as Room;
+                    XYZ center = new XYZ(0, 0, 0);
+                    FamilyInstance roomTag = doc.Create.NewFamilyInstance(center, familySymbol, Autodesk.Revit.DB.Structure.StructuralType.NonStructural);
+
+                }
+                for (int i = 0; i < allRooms.Count; i++)
+                {
+                    for (int j = 0; j < allRooms[i].Count; j++)
+                    {
+
+                       (doc.GetElement(allRooms[i][j]) as Room).Name = $"{i + 1}-{j + 1}";// Вариант, когда нумерация начинается на этаже заново
+                        //(doc.GetElement(allRooms[i][j]) as Room).Name = $"{i + 1}-{(doc.GetElement(allRooms[i][j]) as Room).Number}";//Вариант со сквозной нумерацией по всему проекту
+
+                    }
+                }
+            }
+            transaction.Commit();
+            return Result.Succeeded;
+        }
+
+      
+
+    }
+}
---
+++ FinalPracticeLab_8-Fin_RoomTags/GetAssociatedLevels.cs
@@ -0,0 +1,47 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalPracticeLab_8_Fin_RoomTags
+{
+    public class GetAssociatedLevels
+    {
+        public static List<Level> ToViewPlans(Document doc)
+        {
+            List<Level> ViewAndLevel = new List<Level>();
+            List<Level> levelCollector = new FilteredElementCollector(doc)//находит элементы - уровни
+           .WhereElementIsNotElementType()
+           .OfClass(typeof(Level))
+           .Cast<Level>()
+           .OfType<Level>()
+           .ToList();
+
+            List<ViewPlan> viewCollector = new FilteredElementCollector(doc)//находит элементы - виды, у которых GenLevel не null (иначе исключениев цикле foreach)
+           .WhereElementIsNotElementType()
+           .OfClass(typeof(ViewPlan))
+           .Cast<ViewPlan>()
+           .OfType<ViewPlan>()
+           .Where(x => x.GenLevel != null)
+           .ToList();
+
+
+            foreach (Level level in levelCollector)
+
+            {
+                foreach (ViewPlan view in viewCollector)
+                {
+                    if (view.GenLevel.Id == level.Id)
+                    {
+                        ViewAndLevel.Add(level as Level);
+                        break;
+                    }
+                }
+            }
+
+            return ViewAndLevel;
+        }
+    }
+}
